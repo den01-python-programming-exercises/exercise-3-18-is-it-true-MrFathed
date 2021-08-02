4d575d98a14590ff2833ac06aeea1b59598bfd3d
Justin Turner <jmturner638@gmail.com>
2021-08-02 05:29:26 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    string = input("Give a string:")
+
+    if word == "True":
+        print("You got it right!")
+    else:
+        print("Try again!")
 
 if __name__ == '__main__':
     main()
